e7590eaa4a8dfdda0b1775c37a20ba4078c3db73
matteo.aduso <matteo.a91@gmail.com>
2021-04-28 22:55:45 +0200

    aggiunte immagini nella lista nozze

--- pages/WeddingList.tsx
+++ pages/WeddingList.tsx
@@ -1,4 +1,4 @@
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Image } from "react-bootstrap";
 import useWindowDimensions from "../services/useWindowDimensions";
 
 export default function WeddingList() {
@@ -13,6 +13,10 @@ export default function WeddingList() {
       >
         <p className="display-6">Non abbiamo una lista nozze ma tanti sogni nel cassetto:</p>
         <p className="display-6">IT74X0306905066100000004662</p>
+        <div className="d-flex flex-row align-items-center justify-content-between p-5 w-100">
+          <Image src="house-with-heart.svg" className="m-3" style={{ height: '150px' }}/>
+          <Image src="world-plane.png" className="m-3" style={{ height: '150px' }}/>
+        </div>
       </Col>
     </Row>
   );
